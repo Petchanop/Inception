936bb6b6f9d3f88249e1dbc066322cbbc7dfc3b3
Petchanop <69717257+Petchanop@users.noreply.github.com>
2023-10-03 01:37:11 +0700

    Add files via upload

--- srcs/requirements/wordpress/tools/wordpressSetup.sh
+++ srcs/requirements/wordpress/tools/wordpressSetup.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 if [ ! -f wp-config.php ]; then
-	wget https://wordpress.org/latest.tar.gz
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 	chmod +x wp-cli.phar
-	wp cli update
 	mv wp-cli.phar /usr/local/bin/wp
-	tar -xpvf latest.tar.gz
-	rm -rf latest.tar.gz
-	mkdir /var/www
-	mkdir /var/www/html
-	mkdir -p /var/www/npiya-is.42.fr
-	pwd
-	cd wordpress
-	cp wp-config-sample.php wp-config.php
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config.php
-	sed -i "s/localhost/$MYSQL_HOST/g" wp-config.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
-	cp -r * /var/www/html/
-	mv * /var/www/npiya-is.42.fr/
+	wp cli update
+	cd /var/www/html
+	echo "Download wordpress"
+	wp core download --allow-root
+	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+	echo "Install wordpress"
+	wp core install --url=npiya-is.42.fr --title="npiya-is.42.fr" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+	wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+
+	wp plugin update --all --allow-root
+
+	cd wp-content
+	mkdir uploads
+	chgrp -R www-data uploads/
+	chmod 775 uploads/
 
 	chown -R www-data:www-data /var/www/html
-	chown -R www-data:www-data /var/www/npiya-is.42.fr
 
 fi
+
+/usr/sbin/php-fpm7.4 -F
